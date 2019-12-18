333c2b7f769c49cc317991093628bd4bf9a91cfc
Lucas Lamar <lucass.lamar@gmail.com>
2019-12-18 19:02:09 -0300

    estiliza carrinho

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,13 +11,13 @@ export default function Header() {
   return (
     <Container>
       <Link to="/">
-        <img src={logo} alt="rocktshoes" />
+        <img src={logo} alt="Rocketshoes" />
       </Link>
 
       <Cart to="/cart">
         <div>
-          <strong>meu carinho</strong>
-          <span>itens</span>
+          <strong>Meu carrinho</strong>
+          <span>3 itens</span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
       </Cart>
