b139782c2186c1bea3cd78e6b15c705d62795bdb
Lucas Lamar <lucass.lamar@gmail.com>
2019-12-18 17:50:31 -0300

    estiliza home

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyled from './styles/global';
+import Header from './components/Header';
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <GlobalStyled />
       <Routes />
     </BrowserRouter>
